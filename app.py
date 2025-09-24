118d2e2edf8f14d247ae45406b368921387b9738
Shruti722 <69274928+Shruti722@users.noreply.github.com>
2025-09-24 01:46:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import google.generativeai as genai
 # -------------------------------
 STATE_FILE = "state.json"
 GAME_URL = "https://ai-quiz-game-vuwsfb3hebgvdstjtewksd.streamlit.app/?role=Player"
-QUESTION_TIME = 18
+QUESTION_TIME = 20
 POINTS_PER_QUESTION = 5
 
 # -------------------------------
